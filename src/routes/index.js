4d51b6324017f382b6ef8c9c8678c1affb54196c
yerson <yerssonyrc@gmail.com>
2020-03-01 04:34:25 -0500

    conexion con fire-base exitosa

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,33 @@
 
 const{Router} = require ('express');
 const router = Router();
+const admin = require('firebase-admin')
+
+var serviceAccount = require("../../tutosnode-firebase-adminsdk-uzxzz-8d32d5802c.json");
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://tutosnode.firebaseio.com/'
+});
+
+const db = admin.database();
 
 router.get('/',(req, res)=>{
 //  console.log("index work")
- res.render('index.hbs');
-   
+ res.render('index.hbs')
 });
 
 
 router.post('/new-contact', (req, res) => {
-    console.log(req.body);
-    res.send('receided')
+    const newContact = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        phone: req.body.phone
+    }
+    db.ref('contacts').push(newContact);
+    res.send('recived');
 });
 
 module.exports=router;
