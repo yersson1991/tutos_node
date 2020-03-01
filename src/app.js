e857ed7f43c45129a32c1971315c7b87e3d9a0e8
yerson <yerssonyrc@gmail.com>
2020-02-29 20:37:27 -0500

    configurando server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const morgan = require ('morgan');
+const exphbs= require('express-handlebars');
+
 const app = express();
+
+//setings
+
+
+//middlewaers
+
+
+//routers
+
+
+// static files
 
 module.exports=app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const app = require('./app');
-const port = 4000;
+// const port = 3000;
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(3000);
+console.log('server on port ', 3000);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
