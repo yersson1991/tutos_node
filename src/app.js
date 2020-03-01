fda69f45e79505a6a6042ac958773df00dcd1823
yerson <yerssonyrc@gmail.com>
2020-02-29 22:24:02 -0500

    nodemon instalado y funcionando

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const morgan = require ('morgan');
 const exphbs= require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
 //setings
 app.set('port',process.env.PORT || 4000);
+app.set('views',path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs({
+    defaultLayout:'main',
+    extname:',.hbs'
+}));
+app.set('cer engine', '.hbs');
+
 
 
 //middlewaers
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
 
 
 //routers
-
+app.use(require("./routes/index"));
 
 // static files
+
+app.use(express.static(path.join(__dirname,'public')));
 
 module.exports=app;
